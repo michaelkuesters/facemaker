e91b26d47b0a29001331f841a7ef1462a729cc6e
Michael Küsters <michaelkuesters@hotmail.com>
2015-09-06 16:23:45 +0200

    init

---
+++ faceData/faceframe_renderer.js
@@ -0,0 +1,32 @@
+		function reset()
+		{
+			document.getElementById('Master').innerHTML="";
+			document.getElementById('Master').appendChild(create_divtag({"name":"OuterDiv","id":1,"content":"","style":"ServiceBox"}));
+			document.getElementById('OuterDiv'+1).appendChild(create_divtag({"name":"InnerDiv","id":1 ,"content":"","style":"serviceBoxSub"}));
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Face","src":"images/shim.png", "style":"Face_Pos"}));
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Eyes","src":"images/shim.png", "style":"Eyes_Pos"}));	  
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Mouth","src":"images/shim.png", "style":"Mouth_Pos"}));	  
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Brows","src":"images/shim.png", "style":"Brows_Pos"}));	  					
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Beard","src":"images/shim.png", "style":"Beard_Pos"}));	  					
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Hair","src":"images/shim.png", "style":"Hair_Pos"}));	  
+			document.getElementById('InnerDiv1').appendChild(create_imagetag({"id":1 ,"name":"Gadget","src":"images/shim.png", "style":"Gadget_Pos"}));	  					
+		}
+
+		function create_imagetag(attribs)
+		{
+		var X=document.createElement("img"); 	
+			X.id=""+attribs.name+attribs.id;
+			X.className =attribs.style;
+			X.src=attribs.src;
+			return X;
+		}
+
+		function create_divtag(attribs)
+		{
+		var X=document.createElement("div"); 
+			X.id=""+attribs.name+attribs.id;
+			X.className=attribs.style;
+			X.innerHTML=attribs.content;
+			return X;
+		}
+			
---
+++ faceData/facemaker.js
@@ -0,0 +1,144 @@
+   var i=0;
+   range=3;
+
+   var limits=
+   {
+   		max:
+   		{
+   		Haircolor:4,
+   		Beardcolor:4,
+   		Beard:11,
+   		Hair:9,
+   		Gadget:8,
+   		Brows:3,
+   		Eyes:4,
+   		Mouth:5,
+   		Face:6
+   		},
+   		min:
+   		{
+   		Haircolor:0,
+   		Beardcolor:0,
+   		Beard:0,
+   		Hair:0,
+   		Gadget:0,
+   		Brows:1,
+   		Eyes:1,
+   		Mouth:1,
+   		Face:1
+   		},
+
+    }
+
+   var Colorlist=
+   {
+   	Beard:new Array("black","brown","red","gray","white"),
+   	Hair:new Array("black","brown","red","redbrown","white","yellow"),
+	}
+
+	var face;
+   	function reset_face()
+   	{
+   		face=
+   		{
+   		Haircolor:0,
+   		Beardcolor:0,
+   		Beard:0,
+   		Hair:0,
+   		Gadget:0,
+   		Brows:1,
+   		Eyes:1,
+   		Mouth:1,
+   		Face:1
+    	}
+   	}
+
+   reset_face();
+
+   function faceitem_next(itemType)
+   {
+   	face[itemType] = face[itemType]+1;
+   	if (face[itemType] > limits.max[itemType]) face[itemType]=limits.min[itemType];
+   	var element_text=face[itemType]
+   	if (itemType =="Haircolor")
+   		{element_text=Colorlist.Hair[face.Haircolor]};
+   	if (itemType =="Beardcolor")
+   		{element_text=Colorlist.Beard[face.Beardcolor]};
+
+   	document.getElementById("lbl_"+itemType).innerHTML=element_text;
+
+   	console.debug(face);
+   }
+
+   function prepare_face() {
+
+   var basicShim="images/shim.png";
+	var imageObject =
+      {
+         Face:basicShim,
+         Hair:basicShim,
+         Gadget:basicShim,
+         Beard:basicShim
+      };
+
+	imageObject.Face = 'images/basic-face/basic-face-' + face.Face + '.png';
+
+	if (face.Hair > 0)
+		{
+			imageObject.Hair = "images/hairstyle/hair-" + Colorlist.Hair[face.Haircolor] +"-"+ face.Hair+ ".png";
+		}
+
+	if (face.Gadget > 0)
+		{
+			imageObject.Gadget = 'images/gadget/gadget-' + face.Gadget+ '.png';
+		}
+
+	if (face.Beard > 0)
+		{
+			imageObject.Beard = "images/beards/beard-" + Colorlist.Beard[face.Beardcolor] +"-"+ face.Beard+ ".png";
+		}
+
+	imageObject.Eyes = "images/eyes/eyes-" + face.Eyes+ ".png";
+
+	imageObject.Mouth ="images/mouth/mouth-" + face.Mouth+ ".png";
+		
+   return imageObject;
+	
+   }
+
+
+   function read_jsonface()
+   {
+      face=JSON.parse(document.getElementById("box_json").value);
+      render_face();
+   }
+
+   function render_face()
+   {
+      imageObject=prepare_face();
+      display_face(imageObject);
+      document.getElementById("box_json").value=JSON.stringify(face);
+   }
+
+   function display_face(imageObject)
+   {
+
+   document.getElementById("Face1").src = imageObject.Face;
+   document.getElementById("Hair1").src = imageObject.Hair;
+   document.getElementById("Brows1").src = "images/brows/brows-" + face.Brows+ ".png";
+   document.getElementById("Eyes1").src = "images/eyes/eyes-" + face.Eyes+ ".png";
+   document.getElementById("Mouth1").src = "images/mouth/mouth-" + face.Mouth+ ".png";
+   document.getElementById("Beard1").src = imageObject.Beard;
+   document.getElementById("Gadget1").src = imageObject.Gadget;
+   
+   }
+
+/*     function nextPage() {
+      var printSize=range*range;
+      document.getElementById("pageid").innerHTML= "Current Page: " +(1+ Math.round(i / printSize, 0));
+      var Counter=0;
+      for (Counter = 0; Counter < printSize; Counter++) {   
+      	writeStuff({"Box_ID":Counter,"stuff":myTagVals[i]}); 	
+        }
+      }
+*/
